fb0d636a0e9f5f65c21d447a204145eaa42cc137
yun <yun.mes41@gmail.com>
2021-03-14 16:32:19 +0900

    image api

---
+++ slide_puzzle_api/src/api/image.ts
@@ -0,0 +1,10 @@
+import {Router} from 'express';
+import fs from "fs";
+const router = Router();
+router.get<unknown, Buffer, null, {ignoreDeviceId: string}>(
+  '/image/puzzle',
+  (req,res) => {
+    const buf = fs.readFileSync("../../image/panel.png");
+    res.send(buf);
+    },
+);
